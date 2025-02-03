a87e2423d29fe7885edb11cb5f17fbaa7c5a319f
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2025-02-03 19:42:57 +1300

    style(cart): Delete the commented out redundant code

--- src/main/java/com/qxy/service/CartService.java
+++ src/main/java/com/qxy/service/CartService.java
@@ -1,17 +1,3 @@
-//package com.qxy.service;
-//
-//import com.qxy.model.po.Cart;
-//
-//public interface CartService {
-//    void createCart(Integer userId);
-//    Cart getCart(Integer userId);
-//    void addItem(Integer userId, Integer productId, Integer quantity);
-//    void removeItem(Integer cartItemId);
-//    void updateItemQuantity(Integer cartItemId, Integer quantity);
-//    void deleteCart(Integer cartId);
-//}
-
-
 package com.qxy.service;
 
 import com.qxy.controller.dto.cart.CartDTO;

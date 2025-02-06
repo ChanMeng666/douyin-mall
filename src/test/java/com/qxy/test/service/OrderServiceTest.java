9aea51a36208d481a3a68f4b87216ac41b4dd3ac
mr-zhu <z3453994582@163.com>
2025-02-06 16:58:08 +0800

    fix:修复product类枚举映射失败问题 新增handler处理枚举映射问题

--- src/test/java/com/qxy/test/service/OrderServiceTest.java
+++ src/test/java/com/qxy/test/service/OrderServiceTest.java
@@ -1,9 +1,16 @@
 package com.qxy.test.service;
 
+import com.qxy.dao.ProductDao;
+import com.qxy.dao.dataobject.ProductDO;
+import com.qxy.model.po.Product;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 /**
  * @Author: dawang
@@ -15,4 +22,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OrderServiceTest {
+
 }
